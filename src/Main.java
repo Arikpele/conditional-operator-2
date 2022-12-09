66f3a7a8c3247c85ba1bb14d71396f8d834abf57
Arikpele <79112835522@y.ru>
2022-12-09 21:12:07 +0300

    задание 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+
+    }
+
+    public static void task1() {
+        System.out.println("задача 1");
+
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("задача 2");
+        int clientDeviceYear = 2015;
+        int clientOS = 1;
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if (clientDeviceYear <= 2015 && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        } else if (clientDeviceYear <= 2015 && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        } else {
+            System.out.println("данные введены не правильно");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("задача 3");
+        int year = 2021;
+        if (year %4==0&&year%100!=0||year%400==0){
+                System.out.println(year+"год является високосным");
+            }
+else {
+                System.out.println(year+ "год не является високосным");
+            }
     }
 }
